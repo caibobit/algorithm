91198466d58d4ab37541cfb481bd58d5f15c7af4
caibobit <caibobit@sina.com>
2017-12-04 14:38:46 +0800

    change the log_dir

--- visualization.py
+++ visualization.py
@@ -12,8 +12,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 max_step = 1000  
 learning_rate = 0.001  
 dropout = 0.9  
-data_dir = '/tmp/tensorflow/mnist/input_data'  
-log_dir = 'tmp/tensorflow/mnist/logs/mnist_with_summaries'  
+data_dir = '/root/tensorflow/mycode/Mnist_data'  
+log_dir = '/root/tensorflow/mycode/logs/mnist_with_summary'  
   
 # 使用input_data.read_data_sets下载MNIST数据，并创建Tensorflow的默认Session  
 mnist = input_data.read_data_sets(data_dir, one_hot=True)
